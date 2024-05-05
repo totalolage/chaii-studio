4c40ed46b00be5a055a4db9f20c08f86833231a7
Filip Kalný <filip@kalny.net>
2024-05-05 14:45:51 +0200

    make homepage photo priority

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -4,15 +4,8 @@ import titleImg from "./2024-05-05-010612_002.jpeg";
 export default function Component() {
   return (
     <main>
-    <h1>
-      Chaii Studio :)
-    </h1>
-    <Image
-      src={titleImg}
-      alt="Chaii Studio"
-      placeholder="blur"
-    />
+      <h1>Chaii Studio :)</h1>
+      <Image src={titleImg} alt="Chaii Studio" placeholder="blur" priority />
     </main>
-  )
+  );
 }
-
