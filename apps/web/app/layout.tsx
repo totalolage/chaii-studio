64558a3f89b50879d24afe667814cad463b09921
Filip Kalný <filip@kalny.net>
2024-05-07 13:27:53 +0200

    set up shadcn ui

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,4 +1,6 @@
-import "./globals.css";
+import "normalize.css";
+import "@chaii/tailwind-config/globals.css";
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
---
+++ apps/web/tailwind.config.ts
@@ -0,0 +1,1 @@
+export * from "@chaii/tailwind-config/tailwind.config";
