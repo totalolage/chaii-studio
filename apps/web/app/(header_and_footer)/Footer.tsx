a885ed4364928ce7f2636ddbe1ad6b472c22319c
Filip Kalný <filip@kalny.net>
2024-05-08 16:50:45 +0200

    add email link

--- apps/web/app/(header_and_footer)/Footer.tsx
+++ apps/web/app/(header_and_footer)/Footer.tsx
@@ -3,25 +3,33 @@ import {
   TooltipContent,
   TooltipTrigger,
 } from "@chaii/ui/components/tooltip";
+import { EnvelopeIcon } from "@heroicons/react/16/solid";
 
 export default function Footer() {
   return (
-    <footer className="bg-primary text-white py-4">
-      <div className="container">
+    <footer className="bg-primary text-white py-4 @container">
+      <div className="container flex items-center flex-col @md:flex-row @md:justify-between @md:items-normal">
+
         <Tooltip>
           <TooltipTrigger asChild>
-            <p className="text-center">
+            <span>
               ©&nbsp;
               {new Intl.DateTimeFormat(undefined, {
                 year: "numeric",
               }).format()}{" "}
               Chaii&nbsp;Studio
-            </p>
+            </span>
           </TooltipTrigger>
           <TooltipContent>
             just kidding, intellectual property is a farce
           </TooltipContent>
         </Tooltip>
+
+        <a href="mailto:contact@chaii.studio" className="flex space-x-1 items-center">
+          <EnvelopeIcon className="size-4" />
+          <span>contact@chaii.studio</span>
+        </a>
+          
       </div>
     </footer>
   );
