c480098d1d9ac8d026cd60030bbe77dbc943e412
Filip Kalný <filip@kalny.net>
2024-05-12 22:12:56 +0200

    add accordion menu items to mobile menu

--- packages/ui/tailwind.config.ts
+++ packages/ui/tailwind.config.ts
@@ -1,1 +1,26 @@
-export * from "@chaii/tailwind-config/tailwind.config";
+import tailwindConfig from "@chaii/tailwind-config/tailwind.config";
+import { Config } from "tailwindcss";
+import { merge } from "lodash/fp";
+
+const config = merge(tailwindConfig, {
+  theme: {
+    extend: {
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
+  },
+}) satisfies Config;
+
+export default config;
