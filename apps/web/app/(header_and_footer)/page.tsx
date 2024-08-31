3fdd2941534219dfca5b6c2be4ab413bb0bebb65
Filip Kalný <filip@kalny.net>
2024-08-31 20:39:18 +0200

    some corrections before migrate

--- apps/web/app/(header_and_footer)/page.tsx
+++ apps/web/app/(header_and_footer)/page.tsx
@@ -1,18 +1,7 @@
-import foundersImg from "./founders.jpeg";
-import Image from "next/image";
-
 export default function Component() {
   return (
     <main>
       <section className="relative h-40 w-full">
-        <Image
-          src={foundersImg}
-          alt="Founders"
-          fill
-          priority
-          placeholder="blur"
-          style={{ objectFit: "cover", objectPosition: "0 20%" }}
-        />
       </section>
     </main>
   );
