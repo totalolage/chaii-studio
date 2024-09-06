468eea15324255cc2034878afa9e3e0ce989e9bc
Filip Kalný <filip@kalny.net>
2024-09-06 21:22:41 +0200

    add chaii colors

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -2,8 +2,46 @@ import tailwindConfig from "@chaii/config-tailwind/tailwind.config";
 import { Config } from "tailwindcss";
 import { merge } from "lodash/fp";
 
+const chaiiColors = {
+  black: "#1D1E17",
+  white: "#FFFFF2",
+  blue: {
+    DEFAULT: "#0624ED",
+    50: "#D5DBFE",
+    100: "#C2C9FD",
+    200: "#9AA7FC",
+    300: "#7284FB",
+    400: "#4A61FA",
+    500: "#223EF9",
+    600: "#0624ED",
+    700: "#051CB6",
+    800: "#031380",
+    900: "#020B49",
+    950: "#01072D",
+  },
+  canary: {
+    DEFAULT: "#DFFE5C",
+    50: "#FBFFEA",
+    100: "#F7FFD6",
+    200: "#EFFEAD",
+    300: "#E7FE85",
+    400: "#DFFE5C",
+    500: "#D4FE24",
+    600: "#BCE801",
+    700: "#8FB101",
+    800: "#627901",
+    900: "#354100",
+    950: "#1E2500",
+  },
+};
+
 const config = merge(tailwindConfig, {
   theme: {
+    colors: {
+      ...chaiiColors,
+      foreground: chaiiColors.black,
+      background: chaiiColors.white,
+    },
     extend: {
       fontFamily: {
         sans: ["var(--font-sans)"],
