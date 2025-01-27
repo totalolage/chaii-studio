b8349ef973534a8032a54d011ad2a5ee099e3e24
Filip Kalný <filip@kalny.net>
2025-01-27 20:14:24 +0100

    implement add entity button

---
+++ apps/akl-zalesak-customer-manager/app/components/add-entity-button/AddEntityButton.tsx
@@ -0,0 +1,57 @@
+import { Button, buttonVariants } from "@chaii/ui/components/button";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@chaii/ui/components/popover";
+import { cn } from "@chaii/ui/lib/utils";
+import { Building, PlusIcon, User } from "lucide-react";
+import { Route } from "next";
+import Link from "next/link";
+import { ComponentType, PropsWithChildren } from "react";
+
+export const AddEntityButton = ({ className }: { className?: string }) => {
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button
+          className={cn("bg-primary-500 hover:bg-primary-600", className)}
+        >
+          <PlusIcon className="size-6" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="p-0">
+        <menu>
+          <EntityRow link="/create/customer" Icon={Building}>
+            Create Customer
+          </EntityRow>
+          <EntityRow link="/create/technician" Icon={User}>
+            Create Technician
+          </EntityRow>
+        </menu>
+      </PopoverContent>
+    </Popover>
+  );
+};
+
+const EntityRow = <T extends string>({
+  link,
+  children,
+  Icon,
+}: PropsWithChildren<{
+  link: Route<T>;
+  Icon: ComponentType<{ className?: string }>;
+}>) => (
+  <li>
+    <Link
+      href={link}
+      className={cn(
+        buttonVariants({ variant: "ghost" }),
+        "flex w-full justify-start gap-1",
+      )}
+    >
+      <Icon className="size-4" />
+      <span className="pl-2">{children}</span>
+    </Link>
+  </li>
+);
