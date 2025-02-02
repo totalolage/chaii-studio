9f22af0ef77d61668a7baf1478744ab70e028bfb
Filip Kalný <filip@kalny.net>
2025-02-02 20:36:12 +0100

    update environment variable configuration

--- apps/akl-zalesak-customer-manager/env.ts
+++ apps/akl-zalesak-customer-manager/env.ts
@@ -12,11 +12,8 @@ export const env = createEnv({
   client: {
     NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string(),
   },
-  runtimeEnv: {
-    DATABASE_URL: process.env["DATABASE_URL"],
-    CLERK_SECRET_KEY: process.env["CLERK_SECRET_KEY"],
-    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env[
-      "NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY"
-    ],
+  experimental__runtimeEnv: {
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
+      process.env["NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY"],
   },
 });
