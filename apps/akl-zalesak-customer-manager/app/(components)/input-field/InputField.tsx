9553dbac8715e11554859505d3459f92c447187f
Filip Kalný <filip@kalny.net>
2025-02-06 17:35:23 +0100

    form input serialization

--- apps/akl-zalesak-customer-manager/app/(components)/input-field/InputField.tsx
+++ apps/akl-zalesak-customer-manager/app/(components)/input-field/InputField.tsx
@@ -1,4 +1,4 @@
-import { Control, Path } from "react-hook-form";
+import { Control, FieldPathValue, Path } from "react-hook-form";
 import {
   FormControl,
   FormField,
@@ -9,46 +9,62 @@ import {
 import { Input } from "@chaii/ui/components/input";
 import { cn } from "@chaii/ui/lib/utils";
 import { ComponentProps } from "react";
+import { Merge } from "type-fest";
 
-export const InputField = <Schema extends {}>({
+export const InputField = <Schema extends {}, Name extends Path<Schema>>({
   control,
   type,
   name,
   label,
-}: {
-  control: Control<Schema>;
-  type?: ComponentProps<typeof Input>["type"];
-  name: Path<Schema>;
-  label: string;
-}) => (
-  <FormField
-    control={control}
-    name={name}
-    render={({ field, fieldState }) => (
-      <FormItem className="relative mt-2 space-y-[unset]">
-        <FormControl>
-          <Input
-            {...field}
+  valueSerializer = (value) => value as string,
+  valueDeserializer = (value) => value,
+  ...props
+}: Merge<
+  ComponentProps<typeof Input>,
+  {
+    control: Control<Schema>;
+    type?: ComponentProps<typeof Input>["type"];
+    name: Name;
+    label: string;
+    valueSerializer?: (value: FieldPathValue<Schema, Name>) => string;
+    valueDeserializer?: (value: string) => FieldPathValue<Schema, Name>;
+  }
+>) => {
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field, fieldState }) => (
+        <FormItem className="relative mt-2 space-y-[unset]">
+          <FormControl>
+            <Input
+              {...field}
+              className={cn(
+                "focus:placeholder-[transparent]",
+                fieldState.invalid && "border-destructive",
+              )}
+              value={valueSerializer(field.value ?? undefined)}
+              onChange={(event) => {
+                console.log(event.target.value);
+                field.onChange(valueDeserializer(event.target.value));
+              }}
+              type={type}
+              placeholder=""
+              {...props}
+            />
+          </FormControl>
+          <FormLabel
             className={cn(
-              "focus:placeholder-[transparent]",
-              fieldState.invalid && "border-destructive",
+              "pointer-events-none absolute left-1 bg-white px-1.5 leading-none opacity-100 transition-all center-y",
+              "[input:is(:not(:placeholder-shown),:focus)+&]:bottom-full [input:placeholder-shown:not(:focus)+&]:text-base",
+              fieldState.invalid ? "text-destructive" : "text-muted-foreground",
             )}
-            value={field.value ?? undefined}
-            type={type}
-            placeholder=""
-          />
-        </FormControl>
-        <FormLabel
-          className={cn(
-            "center-y pointer-events-none absolute left-1 bg-white px-1.5 leading-none opacity-100 transition-all",
-            "[input:is(:not(:placeholder-shown),:focus)+&]:bottom-full [input:placeholder-shown:not(:focus)+&]:text-base",
-            fieldState.invalid ? "text-destructive" : "text-muted-foreground",
-          )}
-        >
-          {label}
-        </FormLabel>
-        {fieldState.invalid && <FormMessage />}
-      </FormItem>
-    )}
-  />
-);
+          >
+            {label}
+          </FormLabel>
+          {fieldState.invalid && <FormMessage />}
+        </FormItem>
+      )}
+    />
+  );
+};
